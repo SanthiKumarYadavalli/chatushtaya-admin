f5a096d5c6cad96eb2f48e737fb6bf61d8d6b078
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-12-17 21:05:07 +0530

    feat: add sidebar

---
+++ src/app/admin/layout.js
@@ -0,0 +1,20 @@
+import { SidebarProvider, SidebarTrigger, SidebarInset } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export default function Layout({ children }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <main>
+          <header className={`flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 w-full`}>
+            <div className="flex items-center justify-between w-full">
+              <SidebarTrigger className="ml-2" />
+            </div>
+          </header>
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
---
+++ src/components/app-sidebar.jsx
@@ -0,0 +1,94 @@
+"use client";
+import { Contact, Home, Inbox, LogOut } from "lucide-react"
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar"
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { ModeToggle } from "./mode-toggle"
+import { Separator } from "./ui/separator";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/admin/dashboard",
+    icon: Home,
+  },
+  {
+    title: "Reports",
+    url: "/admin/reports",
+    icon: Inbox,
+  },
+  {
+    title: "Contacts",
+    url: "/admin/contacts",
+    icon: Contact,
+  },
+]
+
+export function AppSidebar() {
+  const pathname = usePathname();
+  return (
+    <Sidebar variant="inset">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild className="h-12">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" className="w-8 h-8"/>
+                <span>Admin</span>
+              </Avatar>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <Separator orientation="horizontal" className="my-2"/>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild isActive={pathname === item.url}>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+              <Separator orientation="horizontal" className="my-2"/>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <button>
+                    <LogOut />
+                    <span>Logout</span>
+                  </button>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+              <ModeToggle />
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
